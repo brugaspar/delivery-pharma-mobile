e06dbe6adebf44fe6baf1aca3f84fb799b414a23
Bruno Gaspar <bruninhoogaspar@gmail.com>
2022-10-10 17:35:53 -0300

    feat: added input component

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,36 @@
 import styled from "styled-components/native";
 import { styles } from "../../globals/styles.global";
 
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 20px;
+`;
+
+export const Username = styled.View`
+  flex: 1;
+`;
+
 export const Title = styled.Text`
-  font-family: ${styles.fonts.bold};
+  font-family: ${styles.fonts.regular};
   font-size: ${styles.sizes.heading};
   color: ${styles.colors.heading};
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${styles.fonts.regular};
+  font-size: ${styles.sizes.highlight};
+  color: ${styles.colors.body};
+`;
+
+export const ProfileButton = styled.TouchableOpacity`
+  background: ${styles.colors.border};
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+
+  align-items: center;
+  justify-content: center;
 `;
